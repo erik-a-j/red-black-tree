c932f896a0be83e48523922eeb3726fd83ce2f7e
Erik Johansson <erik.alexius.johansson@gmail.com>
2026-07-02 17:31:07 +0200

    sync main -> origin/main

--- rbtree2.h
+++ rbtree2.h
@@ -6,6 +6,8 @@
 //#include <iostream>
 #include <memory>
 #include <utility>
+#include <type_traits>
+#include <concepts>
 
 namespace rb {
 
@@ -19,30 +21,95 @@ namespace rb {
  *   same number of black nodes.
  */
 
-namespace Color {
-enum : bool {
-    RED = false, BLACK = true
+enum class Dir : uintptr_t {
+    BIT = 1ULL << 0,
+    LEFT = 0,    // 0
+    RIGHT = BIT, // 1
 };
-using Value = bool;
-}
-namespace Dir {
-enum : bool {
-    LEFT = false, RIGHT = true
+enum class Color : uintptr_t {
+    BIT = 1ULL << 1,
+    RED = 0,     // 0
+    BLACK = BIT, // 2
 };
-using Value = bool;
-}
+
+constexpr uintptr_t operator+(Dir e) noexcept { return static_cast<uintptr_t>(e); }
+constexpr uintptr_t operator+(Color e) noexcept { return static_cast<uintptr_t>(e); }
+constexpr uintptr_t operator~(Dir e) noexcept { return ~static_cast<uintptr_t>(e); }
+constexpr uintptr_t operator~(Color e) noexcept { return ~static_cast<uintptr_t>(e); }
+
+constexpr uintptr_t TAG_MASK = +Dir::BIT | +Color::BIT;
+constexpr uintptr_t PTR_MASK = ~TAG_MASK;
+
+struct Node;
+struct NodePtr {
+    uintptr_t value;
+
+    constexpr NodePtr(uintptr_t v) : value{v} {}
+    constexpr NodePtr(Node* n) : value{reinterpret_cast<uintptr_t>(n)} {}
+
+    NodePtr& setPtr(Node* n) noexcept
+    {
+        value = (value & TAG_MASK) | reinterpret_cast<uintptr_t>(n);
+        return *this;
+    }
+    NodePtr& setDir(Dir e) noexcept
+    {
+        value = (value & ~Dir::BIT) | +e;
+        return *this;
+    }
+    NodePtr& setColor(Color e) noexcept
+    {
+        value = (value & ~Color::BIT) | +e;
+        return *this;
+    }
+
+    Node* node() noexcept { return reinterpret_cast<Node*>(value & PTR_MASK); }
+    Node* operator->() noexcept { return node(); }
+
+    explicit operator uintptr_t() const noexcept { return value; }
+
+    uintptr_t operator|(Dir e) const noexcept { return value | +e; }
+    uintptr_t operator|(Color e) const noexcept { return value | +e; }
+    uintptr_t operator&(Dir e) const noexcept { return value & +e; }
+    uintptr_t operator&(Color e) const noexcept { return value & +e; }
+
+};
+
+constexpr NodePtr NIL = +Color::BLACK;
 
 struct Node {
-    Node* p;
-    Node* child[2];
-    Color::Value color;
+    NodePtr p;
+    NodePtr child[2];
 
-    Node() : p{nullptr}, child{nullptr,nullptr}, color{Color::RED} {}
+    Node() : p{NIL}, child{NIL,NIL} {}
+
+    NodePtr& left() noexcept { return child[+Dir::LEFT]; }
+    NodePtr& right() noexcept { return child[+Dir::RIGHT]; }
+
+    NodePtr& as_child() noexcept { p->child[+dir()]; }
+    NodePtr& as_parent(Dir child_dir) noexcept { child[+child_dir]->p; }
+
+
+    Color color_left() noexcept { return static_cast<Color>(child[+Dir::LEFT] & Color::BIT); }
+    Color color_right() noexcept { return static_cast<Color>(child[+Dir::RIGHT] & Color::BIT); }
+    Color color_p() noexcept { return static_cast<Color>(p & Color::BIT); }
+
+    Dir dir() noexcept { return static_cast<Dir>(p & Dir::BIT); }
+
 };
 
 class TreeBase {
 protected:
+    NodePtr m_root;
 
+    TreeBase() : m_root{NIL} {}
+
+    static void replaceNode(Node* old_node, Node* new_node) noexcept
+    {
+        old_node->as_parent(Dir::LEFT).setPtr(new_node);
+        old_node->as_parent(Dir::RIGHT).setPtr(new_node);
+        old_node->p->child[+old_node->dir()].setPtr(new_node);
+    }
 };
 
 
@@ -52,7 +119,26 @@ class Tree : TreeBase {
     [[no_unique_address]] Cmp m_cmp;
 public:
 
-    bool insert(Node* node);
+    Tree() = default;
+
+    void insert(Node* node)
+    {
+        NodePtr p{NIL};
+        NodePtr curr{m_root};
+
+        while (curr != NIL)
+        {
+            p = curr;
+            int dir = m_cmp(node, toNode(curr));
+            if (dir > 0) curr = toNode(curr)->child[+Dir::RIGHT];
+            else if (dir < 0) curr = toNode(curr)->child[+Dir::LEFT];
+            else
+            {
+                replaceNode(curr, node);
+                break;
+            }
+        }
+    }
 };
 
 } // namespace rb
